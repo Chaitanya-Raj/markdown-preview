54af8b6cb5bf0d7dd2454a4fcdb08175876ae643
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-26 16:16:05 +0530

    implement markdown parsing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Marked from "marked";
+import "./App.css";
 
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    Marked.setOptions({
+      renderer: new Marked.Renderer(),
+      pedantic: false,
+      gfm: true,
+      breaks: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false,
+    });
+    document.querySelector(".output").innerHTML = Marked(markdown);
+  }, [markdown]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Markdown Preview</h1>
       </header>
+      <div className="container">
+        <div className="markdown">
+          <textarea
+            name="md"
+            id="md"
+            cols="30"
+            rows="10"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          ></textarea>
+        </div>
+        <div className="output"></div>
+      </div>
     </div>
   );
 }
